5ddf75fd6c3da8f41d5f4f0b0c678a40e4310285
bsipek07 <borna.sipek@gmail.com>
2025-03-18 20:47:01 +0100

    commit2

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,36 @@ public class Main {
         Scanner input = new Scanner(System.in);
         double[] novcanice = {500,200,100,50,20,10,5};
         double[] kovanice = {2,1,0.50,0.2,0.1,0.05,0.02,0.01};
-        double[] povrat = {0};
 
         System.out.println("Unesite broj");
-        int broj = input.nextInt();
+        double broj = input.nextDouble();
         double tmp=broj;
 
-
+        int j=0;
         for(int i=0;i<novcanice.length;i++){
 
             int cnt=0;
             int cnt2=0;
-            int j=0;
+
             while(tmp-novcanice[i]>=0 ){
                             cnt++;
                             tmp-=novcanice[i];
                 }
-            if(novcanice[i]*cnt>0) System.out.println(novcanice[i]*cnt);
+            if(novcanice[i]*cnt>0)
+                System.out.println(novcanice[i] +" x "+ cnt);
 
-            if(tmp<5){
-                while(tmp-kovanice[j]>0)
+
+            if(tmp<5 && tmp>0){
+                if(tmp-kovanice[j]<0)i=0;
+                while(tmp-kovanice[i]>=0)
                 {
-                    tmp-=kovanice[j];
+                    tmp-=kovanice[i];
                     cnt2++;
-
-                    if(kovanice[j]*cnt2>0)System.out.println(kovanice[j]*cnt2);
-                    j++;
                 }
+                if(kovanice[i]*cnt2>0)
+                    System.out.println(kovanice[i] +" x "+ cnt2);
 
+                j++;
             }
 
             }
